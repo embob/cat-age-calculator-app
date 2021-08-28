8873948b180be85e751dcc0ba1a3314c719d3e21
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-08-28 20:04:10 +0100

    Give FooterIcons a key

--- src/components/FooterIcons/FooterIcons.jsx
+++ src/components/FooterIcons/FooterIcons.jsx
@@ -3,8 +3,8 @@ import "./FooterIcons.css"
 
 export default function FooterIcons({icons}) {
 
-  const footerIcons = icons.map((icon) =>
-    <a className="footer-icons__icon" href={icon.url}>
+  const footerIcons = icons.map((icon, index) =>
+    <a key={index} className="footer-icons__icon" href={icon.url}>
       <img className="footer__icon" src={`images/${icon.name}.svg`} alt={`${icon.name} logo`} />
     </a>
   );
