d349426177be4d978f25f26d900fc4592dee3ac3
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-08-29 22:17:34 +0100

    Add package for better social icons + cleanup old solution

--- src/components/FooterIcons/FooterIcons.jsx
+++ src/components/FooterIcons/FooterIcons.jsx
@@ -1,17 +1,12 @@
 import React from "react";
+import { SocialIcon } from 'react-social-icons';
 import "./FooterIcons.css"
 
 export default function FooterIcons({icons}) {
-
-  const footerIcons = icons.map((icon, index) =>
-    <a key={index} className="footer-icons__icon" href={icon.url}>
-      <img className="footer__icon" src={`images/${icon.name}.svg`} alt={`${icon.name} logo`} />
-    </a>
-  );
-
   return (
     <div className="footer-icons">
-      { footerIcons }
+      <SocialIcon url="https://twitter.com/embobmaria" style={{ height: 40, width: 40 }} />
+      <SocialIcon url="https://github.com/embob/cat-age-calculator-app" style={{ height: 40, width: 40 }} />
     </div>
   )
 }
