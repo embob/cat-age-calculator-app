7f43cc2dd27cb3e6b0fa71338566f3b95c8a3494
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-08-29 21:54:22 +0100

    Remove test logos + add height/width to all img in app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Header.css";
-import logo from './logo2-big.jpg';
+import logo from "./logo.jpg";
 
 export default function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Cat Age Calculator" />
+      <img
+        className="header__logo"
+        src={logo}
+        width="310"
+        height="133"
+        alt="Cat Age Calculator"
+      />
     </header>
   );
 }
--- src/components/LifeStageImage/LifeStageImage.jsx
+++ src/components/LifeStageImage/LifeStageImage.jsx
@@ -5,5 +5,5 @@ export default function LifeStageImage() {
   const { age, lifeStage} = useContext(CalculatorContext);
   if (!age) return null;
   if (age > 116) return <img src={`images/tombstone.jpg`} alt="" />;
-  return <img src={`images/${lifeStage}.jpg`} alt={`a ${lifeStage} cat`}/>;
+  return <img src={`images/${lifeStage}.jpg`} width="488" height="380" alt={`a ${lifeStage} cat`}/>;
 };
