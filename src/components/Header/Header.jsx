ad3abe4585bdf2caa46f416f7955eac384af763a
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-08-29 21:26:16 +0100

    Try inlining image logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from './logo.jpg';
 
 export default function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src={`images/logo.jpg`} alt="Cat Age Calculator" />
+      <img className="header__logo" src={logo} alt="Cat Age Calculator" />
     </header>
   );
 }
